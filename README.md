ddcbae8ddccb637a5188a019e9b8193ef831b5da
mapingchuan <mapingchuan@sensetime.com>
2018-12-25 12:37:59 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Temporal-Shift-Module
-Unofficial implementation for paper `Temporal Shift Module for Efficient Video Understanding`
+
+Unofficial implementation for paper Temporal Shift Module
+for Efficient Video Understanding <b id="tsm">[1](#tsm)</b>.
+
+To use this implementation, simply import the utility with:
+
+```python
+from tsm_util as tsm
+```
+
+Here we use 2D CNN baseline from Temporal Segment Networks
+<b id="tsn">[2](#tsn)</b> with 2D CNN backbone of ResNet
+<b id="resnet">[3](#resnet)</b>.
+
+## Reference
+
+<b id="tsm">[1]</b> [Temporal Shift Module for EfficientVideo Understanding](https://arxiv.org/abs/1811.08383) [back](#tsm)
+
+<b id="tsn">[2]</b> [Temporal Segment Networks: Towards Good Practices for Deep Action Recognition](https://arxiv.org/abs/1608.00859) [back](#tsn)
+
+<b id="resnet">[3]</b> [Deep Residual Learning for Image Recognition](https://arxiv.org/abs/1512.03385) [back](#resnet)
---
+++ tsm_util.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn.functional as F
+
+def tsm(tensor, duration, version='zero'):
+    # tensor [N*T, C, H, W]
+    size = tensor.size()
+    tensor = tensor.view((-1, duration) + size[1:])
+    # tensor [N, T, C, H, W]
+    pre_tensor, post_tensor, peri_tensor = tensor.split([size[1] // 4,
+                                                         size[1] // 4,
+                                                         size[1] // 2], dim=2)
+    if version == 'zero':
+        pre_tensor  = F.pad(pre_tensor, (0, 0, 0, 0, 0, 0, 1, 0))[:,  :-1, ...]
+        post_tensor = F.pad(pre_tensor, (0, 0, 0, 0, 0, 0, 0, 1))[:, 1:  , ...]
+    elif version == 'circulant':
+        pre_tensor  = torch.cat((pre_tensor [:, -1:  , ...],
+                                 pre_tensor [:,   :-1, ...]), dim=1)
+        post_tensor = torch.cat((post_tensor[:,  1:  , ...],
+                                 post_tensor[:,   :1 , ...]), dim=1)
+    else:
+        raise ValueError('Unknown TSM version: {}'.format(version))
+    return torch.cat((pre_tensor, post_tensor, peri_tensor), dim=2).view(size)
